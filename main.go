f67583c4ccaa1eef77d621d2e96134b653c05746
miketitus <mike@mike-titus.com>
2018-04-11 14:11:42 -0700

    decoding body

--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	// "fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/userbot", parseEmail)
-	err := http.ListenAndServeTLS(":8443", "userbot.crt", "userbot.key", nil)
+	err := http.ListenAndServe(":8443", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("ListenAndServe", err)
 	}
 }
 
 func parseEmail(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
+	bodyBytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal("ioutil.ReadAll", err)
+	}
+	body := string(bodyBytes)
+	log.Println("********")
 	log.Println(req.Header)
-	log.Print('\n')
-	log.Println(req.Body)
-	log.Print('\n')
+	log.Println("********")
+	log.Println(body)
+	log.Println("********")
 	w.WriteHeader(http.StatusOK)
 }
