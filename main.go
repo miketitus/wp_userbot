75dd263376290102948b386abf31098bcd64d6c4
miketitus <mike@mike-titus.com>
2018-04-11 14:40:27 -0700

    stash work

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"net/QueryUnescape"
 	"net/http"
 )
 
@@ -16,15 +17,19 @@ func main() {
 
 func parseEmail(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	bodyBytes, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		log.Fatal("ioutil.ReadAll", err)
+	bodyBytes, err1 := ioutil.ReadAll(req.Body)
+	if err1 != nil {
+		log.Fatal("ioutil.ReadAll", err1)
 	}
-	body := string(bodyBytes)
+	bodyString := string(bodyBytes)
+	body, err2 := QueryUnescape(bodyString)
+	if err2 != nil {
+		log.Fatal("QueryUnescape", err2)
+	}
 	log.Println("********")
 	log.Println(req.Header)
 	log.Println("********")
-	log.Println(body)
+	log.Println(bodyString)
 	log.Println("********")
 	w.WriteHeader(http.StatusOK)
 }
