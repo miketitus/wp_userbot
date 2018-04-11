7a356d8f58f75120c7e219d57478cb2929aef3ce
miketitus <mike@mike-titus.com>
2018-04-11 16:34:23 -0700

    stash parsing work

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"regexp"
+	"strings"
 )
 
+type user struct {
+	first string
+	last  string
+	email string
+}
+
+var validSender = ""
+
 func main() {
+	/* read env settings */
+	validSender = os.Getenv("USERBOT_SENDER")
+	fmt.Printf("validSender: %s\n", validSender)
+	/* lauch http server */
 	http.HandleFunc("/userbot", parseEmail)
 	err := http.ListenAndServe(":8443", nil)
 	if err != nil {
@@ -16,7 +32,11 @@ func main() {
 }
 
 func parseEmail(w http.ResponseWriter, req *http.Request) {
+	/* acknowledge POST from Mailgun  */
 	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+
+	/* decode body */
 	bodyBytes, err1 := ioutil.ReadAll(req.Body)
 	if err1 != nil {
 		log.Fatal("ioutil.ReadAll", err1)
@@ -26,10 +46,37 @@ func parseEmail(w http.ResponseWriter, req *http.Request) {
 	if err2 != nil {
 		log.Fatal("QueryUnescape", err2)
 	}
-	log.Println("********")
-	log.Println(req.Header)
-	log.Println("********")
-	log.Println(body)
-	log.Println("********")
-	w.WriteHeader(http.StatusOK)
+
+	sender, err3 := getSender(body)
+	if err3 != nil {
+		illegalSenderAlert(err3)
+	}
+	fmt.Printf("sender = %s\n", sender)
+
+	recipients := getRecipients(body)
+	fmt.Printf("recipients = %s\n", recipients)
+}
+
+func getSender(body string) (string, error) {
+	senderRE := regexp.MustCompile("from=(.*)&")
+	sender := senderRE.FindString(body)[5:]
+	if sender != validSender {
+		return "", fmt.Errorf("Illegal sender: %s", sender)
+	}
+	return sender, nil
+}
+
+func getRecipients(body string) []string {
+	recipientRE := regexp.MustCompile("To=(.*)&")
+	raw := recipientRE.FindString(body)
+	recipients := strings.Split(raw[3:], ", ")
+	return recipients
+}
+
+func createUsers(recipients []string) {
+
+}
+
+func illegalSenderAlert(err error) {
+	log.Fatal("illegalSenderAlert", err)
 }
