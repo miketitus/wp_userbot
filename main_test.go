510545dd559ae252068a15910a49f45eb455c118
miketitus <mike@mike-titus.com>
2018-04-15 14:09:17 -0700

    add invalid email

--- main_test.go
+++ main_test.go
@@ -18,7 +18,7 @@ func TestIsValidSender(t *testing.T) {
 
 func TestIsValidEmail(t *testing.T) {
 	validEmails := []string{"abc@xyz.com", "abc+@xyz.us", "abc@wa.us"}
-	inValidEmails := []string{"abcxyz.com", "abc@@xyz.us", "abc@waus", "abc@waus."}
+	inValidEmails := []string{"abcxyz.com", "abc@@xyz.us", "abc@waus", "abc@waus.", "abcxyz"}
 	for _, e := range validEmails {
 		if !IsValidEmail(e) {
 			t.Errorf("'%s' was declared invalid", e)
