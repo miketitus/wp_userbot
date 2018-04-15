22c46f963333acdf4035ea13adc8a4df870fcf84
miketitus <mike@mike-titus.com>
2018-04-15 16:01:35 -0700

    test isUserBot

--- main_test.go
+++ main_test.go
@@ -4,15 +4,27 @@ import (
 	"testing"
 )
 
-func TestIsValidSender(t *testing.T) {
-	for _, s := range mgValidSenders {
-		if !IsValidSender(s) {
+func TestIsAdmin(t *testing.T) {
+	for _, s := range mgAdmins {
+		if !isAdmin(s) {
 			t.Errorf("'%s' was declared invalid", s)
 		}
 	}
 	s := "Invalid"
-	if IsValidSender(s) {
+	if isAdmin(s) {
 		t.Errorf("'%s' was declared valid", s)
+	}
+}
+
+func TestIsUserBot(t *testing.T) {
+	s := make([]string, 1, 1)
+	s[0] = mgUserBot
+	if !isUserBot(s) {
+		t.Errorf("'%s' was declared to not be a userbot", s)
+	}
+	s[0] = "foo@abc.xyz"
+	if isUserBot(s) {
+		t.Errorf("'%s' was declared to be a userbot", s)
 	}
 }
 
@@ -20,12 +32,12 @@ func TestIsValidEmail(t *testing.T) {
 	validEmails := []string{"abc@xyz.com", "abc+@xyz.us", "abc@wa.us"}
 	inValidEmails := []string{"abcxyz.com", "abc@@xyz.us", "abc@waus", "abc@waus.", "abcxyz"}
 	for _, e := range validEmails {
-		if !IsValidEmail(e) {
+		if !isValidEmail(e) {
 			t.Errorf("'%s' was declared invalid", e)
 		}
 	}
 	for _, e := range inValidEmails {
-		if IsValidEmail(e) {
+		if isValidEmail(e) {
 			t.Errorf("'%s' was declared valid", e)
 		}
 	}
