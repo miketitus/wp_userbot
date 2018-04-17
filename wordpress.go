6bccf597d249763de30243af373f30a7e8526be1
miketitus <mike@mike-titus.com>
2018-04-16 19:16:57 -0700

    remove go-wordpress

--- wordpress.go
+++ wordpress.go
@@ -1,46 +1,24 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/robbiet480/go-wordpress"
 )
 
 var wpBaseURL, wpPassword, wpUser string
-var wpClient *wordpress.Client
-var wpContext context.Context
 
 func initWordPress() {
 	wpBaseURL = fmt.Sprintf("https://%s/", os.Getenv("WP_BASE_URL"))
 	wpPassword = os.Getenv("WP_PASSWORD")
 	wpUser = os.Getenv("WP_USER")
-	wpContext = context.Background()
-	transport := wordpress.BasicAuthTransport{
-		Username: wpUser,
-		Password: wpPassword,
-	}
-	var err error
-	wpClient, err = wordpress.NewClient(wpBaseURL, transport.Client())
-	if err != nil {
-		log.Fatal("wordpress.NewClient", err)
-	}
 }
 
-/*func userExists(email string) bool {
+func callWP(api, opts string) (*http.Response, error) {
 	if wpBaseURL == "" {
 		initWordPress()
 	}
-	users, resp, err := wpClient.Users.List(wpContext, "search="+email)
-}*/
-
-func callWP(api, opts string) (*http.Response, error) {
-	wpBaseURL = fmt.Sprintf("https://%s/", os.Getenv("WP_BASE_URL"))
-	wpPassword = os.Getenv("WP_PASSWORD")
-	wpUser = os.Getenv("WP_USER")
 	url := wpBaseURL + "wp-json/wp/v2/" + api
 	if opts != "" {
 		url = url + "?" + opts
--- wordpress_test.go
+++ wordpress_test.go
@@ -4,14 +4,6 @@ import (
 	"testing"
 )
 
-func TestInitWordPress(t *testing.T) {
-	initWordPress()
-	_, _, err := wpClient.Users.Me(wpContext, nil)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
 func TestCallWP(t *testing.T) {
 	// test with tags route because it returns minimal data
 	_, err := callWP("tags", "")
