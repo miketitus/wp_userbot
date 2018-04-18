ac8b2c414ffcc0bf92914a9629f8f6404e11b8d5
miketitus <mike@mike-titus.com>
2018-04-18 12:45:21 -0700

    add TestUserExists()

--- wordpress_test.go
+++ wordpress_test.go
@@ -11,3 +11,18 @@ func TestCallWP(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestUserExists(t *testing.T) {
+	exists, err := userExists("mike@mike-titus.com")
+	if err != nil {
+		t.Error(err)
+	} else if !exists {
+		t.Error("user should have been found")
+	}
+	exists, err = userExists("nobody@nowhere.xyz")
+	if err != nil {
+		t.Error(err)
+	} else if exists {
+		t.Error("user should not have been found")
+	}
+}
