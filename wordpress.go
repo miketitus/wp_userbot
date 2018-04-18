9df2d3c705a447d58e9b036e3688e2a16e33e0f5
miketitus <mike@mike-titus.com>
2018-04-18 15:59:55 -0700

    complete userExists(), document methods

--- wordpress.go
+++ wordpress.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
+
+// User contains parsed user JSON returned by WordPress
+type User struct {
+	ID          int32  `json:"id"`
+	DisplayName string `json:"name"`
+	Username    string `json:"slug"`
+}
 
 var wpBaseURL, wpPassword, wpUser string
 
+// initWordPress reads env vars (via lazy loading)
 func initWordPress() {
 	wpBaseURL = fmt.Sprintf("https://%s/", os.Getenv("WP_BASE_URL"))
 	wpPassword = os.Getenv("WP_PASSWORD")
 	wpUser = os.Getenv("WP_USER")
 }
 
+// callWP sends a generic HTTP GET to the WordPress API
 func callWP(api, opts string) (*http.Response, error) {
 	if wpBaseURL == "" {
 		initWordPress()
@@ -24,7 +34,6 @@ func callWP(api, opts string) (*http.Response, error) {
 	if opts != "" {
 		url = url + "?" + opts
 	}
-	// TODO log.Printf("URL: %s\n", url)
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal("http.NewRequest", err)
@@ -36,12 +45,33 @@ func callWP(api, opts string) (*http.Response, error) {
 	return client.Do(request)
 }
 
+// userExists determines whether a user account already exists (based on email address)
 func userExists(email string) (bool, error) {
 	response, err := callWP("users", "search="+email)
 	if err != nil {
 		return false, err
 	}
-	body, _ := getResponseBody(response)
-	// TODO log.Println(body)
-	return strings.Contains(body, "id"), nil
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("ioutil.ReadAll - %s\n", err)
+		return false, err
+	}
+	var users []User
+	err = json.Unmarshal(body, &users)
+	if err != nil {
+		log.Printf("unmarshall: %s\n", err)
+		return false, err
+	}
+	return len(users) > 0, nil
+}
+
+// TODO
+func createUser(first, last, email string) (bool, error) {
+	exists, err := userExists(email)
+	if err != nil {
+		return false, err
+	} else if exists {
+		return false, nil
+	}
+	return true, nil
 }
