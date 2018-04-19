94d851e87d1895277ba327f793a652fde2b65327
miketitus <mike@mike-titus.com>
2018-04-19 13:33:36 -0700

    update wpAPI, save partial createUser()

--- wordpress.go
+++ wordpress.go
@@ -3,17 +3,24 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // User contains parsed user JSON returned by WordPress.
 type User struct {
-	ID          int32  `json:"id"`
-	DisplayName string `json:"name"`
-	Username    string `json:"slug"`
+	Context     string `json:"context,omitempty"`
+	ID          int32  `json:"id,omitempty"`
+	Username    string `json:"username,omitempty"`
+	DisplayName string `json:"name,omitempty"`
+	FirstName   string `json:"first_name,omitempty"`
+	LastName    string `json:"last_name,omitempty"`
+	Email       string `json:"email,omitempty"`
+	Password    string `json:"password,omitempty"`
 }
 
 var wpBaseURL, wpPassword, wpUser string
@@ -26,15 +33,19 @@ func initWordPress() {
 }
 
 // wpAPI sends a generic HTTP to the WordPress API.
-func wpAPI(method, route, opts string) (*http.Response, error) {
+func wpAPI(method, route, data string) (*http.Response, error) {
+	var body io.Reader
 	if wpBaseURL == "" {
 		initWordPress()
 	}
 	url := wpBaseURL + "wp-json/wp/v2/" + route
-	if opts != "" {
-		url = url + "?" + opts
+	if method == "GET" && data != "" {
+		url = url + "?" + data
+	} else if method == "PUT" && data != "" {
+		body = strings.NewReader(data)
 	}
-	request, err := http.NewRequest(method, url, nil)
+	log.Printf("url: %s\n", url)
+	request, err := http.NewRequest(method, url, body)
 	if err != nil {
 		log.Fatal("http.NewRequest", err)
 	}
@@ -49,11 +60,12 @@ func wpAPI(method, route, opts string) (*http.Response, error) {
 func userExists(email string) (bool, error) {
 	response, err := wpAPI("GET", "users", "search="+email)
 	if err != nil {
+		log.Printf("client.Do: %s\n", response.Header)
 		return false, err
 	}
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Printf("ioutil.ReadAll - %s\n", err)
+		log.Printf("ioutil.ReadAll: %s\n", err)
 		return false, err
 	}
 	var users []User
@@ -67,11 +79,32 @@ func userExists(email string) (bool, error) {
 
 // TODO
 func createUser(first, last, email string) (bool, error) {
+	// ensure user doesn't already exist
 	exists, err := userExists(email)
 	if err != nil {
 		return false, err
 	} else if exists {
 		return false, nil
 	}
+	// build user object
+	user := User{FirstName: first, LastName: last, Email: email}
+	user.Context = "edit" // TODO
+	user.Username = strings.ToLower(first[:1] + last)
+	user.DisplayName = first + ` ` + last
+	user.Password = "aBcDeFgHiJ"
+	j, err := json.Marshal(user)
+	log.Printf("user: %s\n", j)
+	// send user to WP
+	response, err := wpAPI("PUT", "users", string(j))
+	if err != nil {
+		log.Printf("client.Do: %s\n", err)
+		return false, err
+	}
+	log.Printf("header: %s\n", response.Header)
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("ioutil.ReadAll: %s\n", err)
+	}
+	log.Printf("body: %s", body)
 	return true, nil
 }
